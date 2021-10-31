2e20b18dfb95ed1e12a7a456a4186adc0d4dd5b0
theo82 <theoaristi53@gmail.com>
2021-10-31 17:53:42 +0200

    Handling POST Requests

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+// A step a request goes through before it gets processed.
+app.use(express.json());
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello from the server', app: 'Natours' });
 // });
@@ -10,17 +12,39 @@ const app = express();
 //   res.send('You can post to this endpoint...');
 // });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
-      tours: tours
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
     }
-  })
-})
+  );
+});
 
 const port = 3000;
 app.listen(port, () => {
