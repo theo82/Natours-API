fa9c29cbbf43e9bbe3c09470e9f4a702e30337d8
theo82 <theoaristi53@gmail.com>
2021-11-07 17:49:53 +0200

    Using 3rd-Party Middleware

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const morgan = require('morgan');
 
+// 1) MIDDLEWAREs
 // A step a request goes through before it gets processed.
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
@@ -19,6 +22,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// 2) ROUTE HANDLERS
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -102,6 +106,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTES
 app.route('/api/v1/tours/').get(getAllTours).post(createTour);
 
 app
@@ -110,6 +115,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// 4) START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
