5f92480f78dcdf50e188fc0b7d4895d36ed5ab01
theo82 <theoaristi53@gmail.com>
2021-11-28 17:25:01 +0200

    Creating a Simple Tour Model

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
-// console.log(process.env);
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('DB connection successful!');
@@ -21,6 +22,26 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  rating: Number,
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
