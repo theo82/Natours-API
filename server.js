340e76a6703f62edd0b16ceeb6c6c4989195a428
theo82 <theoaristi53@gmail.com>
2021-12-03 17:20:16 +0200

    Refactoring for MVC

--- server.js
+++ server.js
@@ -10,50 +10,18 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    console.log('DB connection successful!');
+  .then((con) => {
+    console.log('DB connections successfully !!');
   })
   .catch((err) => {
     console.log(err);
-  });
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  rating: 4.5,
-  price: 30,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('Error');
   });
 
 const port = process.env.PORT || 3000;
